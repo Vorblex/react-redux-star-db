f8bc29215dcf64bc7ececfd65b15a1a72917906e
vorblex <vorblex@gmail.com>
2020-01-06 00:49:22 +0200

    Index.js cleaned

--- src/index.js
+++ src/index.js
@@ -2,23 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
-import SwapiService from './services/swapi-service'
 
 import './bootstrap.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-const swapi = new SwapiService()
-
-swapi.getAllPlanets()
-  .then( element => {
-    element.forEach(element => {
-      // console.log( 'Planet ', element);
-    });
-    }
-  )
-// swapi.getPerson('4')
-//   .then( persone => {
-//       console.log(persone);
-//     }
-//   )
