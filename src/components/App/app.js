837805976b7af1aa3436616c6447ef79fe767148
vorblex <vorblex@gmail.com>
2020-01-12 16:07:10 +0200

    Router installed

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
+// import { BrowserRouter } from 'react-router-dom'
 
 import Header from '../Header'
 import RandomPlanet from '../RandomPlanet'
 import ErrorBoundry from '../ErrorBoundry'
 import ErrorIndicator from '../ErrorIndicator'
 import SwapiService from '../../services/swapi-service'
-import DummySwapiService from '../../services/dummy-swapi-service';
+import DummySwapiService from '../../services/dummy-swapi-service'
 import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages'
 
-import { SwapiServiceProvider } from '../SwapiServiceContext';
+import { SwapiServiceProvider } from '../SwapiServiceContext'
 
 import './app.css'
 
